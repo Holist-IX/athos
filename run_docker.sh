d44e73cda891f2fad6799486ad96828aa27cead1
Christoff Visser <christoffvis@gmail.com>
2021-03-01 03:25:56 +0000

    Cleaned up docker compose

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 cd `dirname "$0"`
+URGEDIR=/home/ixpman/code/urge
 
 if [ -n "$1" ]; then
     if [ "$1" == 'urge' ]; then
-        docker-compose run -v $URGEDIR:/urge --entrypoint /mixtt/docker/entry_urge.sh mixtt .
+        docker-compose run -v $URGEDIR:/urge --entrypoint /athos/docker/entry_urge.sh athos .
     else
-    docker-compose run --entrypoint /mixtt/docker/entry.sh mixtt .
+        docker-compose run --entrypoint /athos/docker/entry.sh athos .
     fi
 else
-    docker-compose run mixtt .
+    docker-compose run athos .
 fi
