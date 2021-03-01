283caf32035d0a5a96d4955df27152af9b3ea6bd
Christoff Visser <christoffvis@gmail.com>
2021-03-01 02:47:12 +0000

    Fixed refrences in docker entry points

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OUT_FILE=/mixtt/ixpman_files/output.txt
+OUT_FILE=/athos/ixpman_files/output.txt
 
 service openvswitch-switch start &> $OUT_FILE
 ovs-vsctl set-manager ptcp:6640 &>> $OUT_FILE
@@ -9,9 +9,9 @@ faucet &>> $OUT_FILE &
 
 sleep 1
 
-cd /mixtt
+cd /athos
 
-mixtt &>> $OUT_FILE
+athos &>> $OUT_FILE
 cd ixpman_files
 python3 parser.py 2>&1 | tee -a $OUT_FILE
 NOW=`date +%Y_%m_%d_%H_%M_%S`
