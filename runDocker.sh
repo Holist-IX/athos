be08aad944ebf4aee9220d48b14f214725617323
Chris Visser <44282262+Belthazaar@users.noreply.github.com>
2021-03-18 15:22:39 +0900

    Clean up docker entry scripts and docker compose (#8)
    * Made docker compose config more clear
    
    * Changed athos to urge to be it's own service in docker compose

--- runDocker.sh
+++ runDocker.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 cd `dirname "$0"`
-URGEDIR=/home/ixpman/code/urge
 
 if [ -n "$1" ]; then
     if [ "$1" == 'urge' ]; then
-        docker-compose run -v $URGEDIR:/urge --entrypoint /athos/docker/entry_urge.sh athos .
+        docker-compose run --entrypoint /athos/docker/entry_urge.sh athosurge .
     else
-        docker-compose run --entrypoint /athos/docker/entry.sh athos .
+        docker-compose run athos .
     fi
 else
-    docker-compose run athos .
+    docker-compose run athos --entrypoint /athos/docker/entry_no_output.sh
 fi
