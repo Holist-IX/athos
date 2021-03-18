a957824ba9273a2b726f8639bd9e59aa98f838b0
Christoff Visser <christoffvis@gmail.com>
2021-03-18 06:13:36 +0000

    Changed athos to urge to be it's own service in docker compose

--- runDocker.sh
+++ runDocker.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 cd `dirname "$0"`
-URGEDIR=/home/ixpman/code/urge
 
 if [ -n "$1" ]; then
     if [ "$1" == 'urge' ]; then
-        docker-compose run -v $URGEDIR:/urge --entrypoint /athos/docker/entry_urge.sh athos .
+        docker-compose run --entrypoint /athos/docker/entry_urge.sh athosurge .
     else
         docker-compose run athos .
     fi
