d1f16bdbb0c7338487fe18cc7c0d1d6fca55f101
Christoff Visser <christoffvis@gmail.com>
2021-03-18 06:02:10 +0000

    Made docker compose config more clear

--- runDocker.sh
+++ runDocker.sh
@@ -6,8 +6,8 @@ if [ -n "$1" ]; then
     if [ "$1" == 'urge' ]; then
         docker-compose run -v $URGEDIR:/urge --entrypoint /athos/docker/entry_urge.sh athos .
     else
-        docker-compose run --entrypoint /athos/docker/entry.sh athos .
+        docker-compose run athos .
     fi
 else
-    docker-compose run athos .
+    docker-compose run athos --entrypoint /athos/docker/entry_no_output.sh
 fi
