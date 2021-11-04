2146038245ac68390f5273d8396ac018f3e19813
Christoff Visser <christoffvis@gmail.com>
2021-11-04 18:33:37 +0900

    Started inclusion of cerberus into docker

--- docker/entry.sh
+++ docker/entry.sh
@@ -3,7 +3,11 @@ OUT_FILE=/athos/ixpman_files/output.txt
 
 service openvswitch-switch start 2>&1 | tee $OUT_FILE
 ovs-vsctl set-manager ptcp:6640 2>&1 | tee -a $OUT_FILE
-faucet 2>&1 | tee -a $OUT_FILE &
+if [ -n "$1" ] && [ "$1" == 'faucet' ]; then
+    faucet 2>&1 | tee -a $OUT_FILE &
+else
+    cerberus-controller 2>&1 | tee -a $OUT_FILE &
+fi
 sleep 1s
 athos 2>&1 | tee -a $OUT_FILE
 
--- docker/install_athos.sh
+++ docker/install_athos.sh
@@ -3,16 +3,25 @@
 apt update
 apt install -y --no-install-recommends git openvswitch-switch iputils-ping mininet net-tools gcc iproute2
 apt upgrade -y
-pip3 -q --no-cache-dir install --upgrade pip wheel setuptools mininet faucet
+pip3 -q --no-cache-dir install --upgrade pip wheel setuptools mininet
+
+
+
+if [ -n "$1" ] && [ "$1" == 'faucet' ]; then
+        pip3 -q --no-cache-dir install --upgrade faucet
+        cp /athos
+else
+    pip3 -q --no-cache-dir install --upgrade cerberus-controller
+    cp /athos/etc/athos/topology.json /etc/cerberus/topology.json
+fi
 pip3 install .
 ln /bin/sed /usr/bin/sed
 
-
-cp /athos/etc/faucet/faucet.yaml /etc/faucet/faucet.yaml
 mkdir /etc/athos
 mkdir /var/log/athos
 cp /athos/etc/athos/topology.json /etc/athos/topology.json
 cp /athos/etc/athos/umbrella.json /etc/athos/umbrella.json
+# Needed to make tcpdump working within docker
 mv /usr/sbin/tcpdump /usr/bin/tcpdump
 ln -s /usr/bin/tcpdump /usr/sbin/tcpdump
 
