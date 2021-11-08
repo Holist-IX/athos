0b24d0dcf8d4d571430cfd11b63e5973e8a83fef
Christoff Visser <christoffvis@gmail.com>
2021-11-08 10:40:28 +0900

    Add installing cerberus from test pypi

--- docker/install_athos.sh
+++ docker/install_athos.sh
@@ -3,16 +3,20 @@
 apt update
 apt install -y --no-install-recommends git openvswitch-switch iputils-ping mininet net-tools gcc iproute2
 apt upgrade -y
-pip3 -q --no-cache-dir install --upgrade pip wheel setuptools mininet
+pip3 -q --no-cache-dir install --upgrade pip wheel setuptools mininet ryu>=4.34
 
 
 
 if [ -n "$1" ] && [ "$1" == 'faucet' ]; then
         pip3 -q --no-cache-dir install --upgrade faucet
-        cp /athos
+        cp /athos/etc/faucet /etc/faucet
 else
-    pip3 -q --no-cache-dir install --upgrade cerberus-controller
-    cp /athos/etc/athos/topology.json /etc/cerberus/topology.json
+    # pip3 -q --no-cache-dir install --upgrade cerberus-controller
+    pip3 install -q --no-cache-dir install eventlet==0.30.2
+    pip3 install -q --no-cache-dir --index-url https://test.pypi.org/simple cerberus-controller
+    cp -r /usr/local/etc/cerberus /etc/cerberus
+    mkdir /etc/cerberus/ /var/log/cerberus /etc/cerberus/rollback /etc/cerberus/failed
+    touch /var/log/cerberus/cerberus.log
 fi
 pip3 install .
 ln /bin/sed /usr/bin/sed
