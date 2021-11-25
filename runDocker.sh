2095dbfae13309ca6035d4895c7c1f37cfd85060
Christoff Visser <christoffvis@gmail.com>
2021-11-25 15:12:57 +0900

    Updated docker entry points

--- runDocker.sh
+++ runDocker.sh
@@ -3,10 +3,12 @@ cd `dirname "$0"`
 
 if [ -n "$1" ]; then
     if [ "$1" == 'urge' ]; then
-        docker-compose run --entrypoint /athos/docker/entry_urge.sh athosUrge .
-    else
+        docker-compose run athos_urge .
+    elif [ "$1" == 'no_output' ]; then
         docker-compose run athos .
+    elif [ "$1" == 'faucet' ]; then
+        docker-compose run athos_faucet .
     fi
 else
-    docker-compose run athos --entrypoint /athos/docker/entry_no_output.sh
+    docker-compose run athos .
 fi
